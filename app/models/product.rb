47aa679426edc8e6b5862cfba6ada9731816c23d
Ubuntu <vagrant@Base-Box>
2017-11-22 20:44:26 +0000

    Regular commit to keep project up to date

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,8 @@
 class Product < ApplicationRecord
 has_many :customers
 belongs_to :category
+
+mount_uploader :image, ImageUploader
 validates :name, :description, :price, :stock_quantity, :released, presence: true
 
 end
